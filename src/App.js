bf44415516b937cb81a999d44d1aa7a297929b52
Andrew <83809296+YoungerCper@users.noreply.github.com>
2021-09-21 16:47:42 +0300

    NavigationBar_2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import NavigationBar from './NavigationBar/NavigationBar.js';
+import Card from './Card/Card';
 
 function App()
 {
 
     return (
         <div className='main'>
+
             <NavigationBar></NavigationBar>
+
+            <Card></Card>
         </div>
     );
 }
